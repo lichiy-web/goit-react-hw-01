4768eb897bf8ae767f50e43d3ad4ba888231c7c8
Lichiy <lichiy.web.dwlp@gmail.com>
2025-01-10 03:10:48 +0200

    add jsdoc description to normalizeProfileProps method

--- src/components/Profile/normalizeProfileProps.js
+++ src/components/Profile/normalizeProfileProps.js
@@ -1,4 +1,10 @@
-export default function normalizeProfileProps (userData) {
+/**
+ * The method brings the names of the Profile component's properties to a unified form.
+ * In essence, this method is an adapter between the database and the component.
+ * @param {Object} userData 
+ * @returns {Object} 
+ */
+export default function normalizeProfileProps(userData) {
   let normData = {};
   for (let [key, value] of Object.entries(userData)) {
     switch (key) {
