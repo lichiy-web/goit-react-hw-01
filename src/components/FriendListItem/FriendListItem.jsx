15bafc7cd2876fed7442061703f5908f18d65e93
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-01-11 11:04:50 +0200

    start to stylize the FriendList component.

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,16 @@
 import css from './FriendListItem.module.css';
 import clsx from 'clsx';
-console.log('FriendListItem css:', css);
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
-  console.log(avatar, name, isOnline);
   return (
-    <div className="friend-list-item">
+    <li className={css['friend-list-item']}>
       <img
         src={avatar}
         alt="Avatar"
         width="48"
-        className="friend-list-item-avatar"
+        className={css['friend-list-item-avatar']}
       />
-      <p className="friend-list-item-name">{name}</p>
+      <p className={css['friend-list-item-name']}>{name}</p>
       <p
         className={clsx(css['friend-list-item-status'], {
           [css['is-online']]: isOnline,
@@ -20,7 +18,7 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
       >
         {isOnline ? 'on' : 'off'}line
       </p>
-    </div>
+    </li>
   );
 };
 
